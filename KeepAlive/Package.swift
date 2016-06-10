c63ba0f56cf5c62aaddbd57a461a9fd872dbc9bd
Joe <joe@iachieved.it>
2016-06-09 21:56:17 -0500

    Switch to proper package

--- KeepAlive/Package.swift
+++ KeepAlive/Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "KeepAlive",
     dependencies:[
-      //.Package(url:"https://github.com/iachievedit/MQTT", majorVersion:0),
-      .Package(url:"../../MQTT", majorVersion:0),
+      .Package(url:"https://github.com/iachievedit/MQTT", majorVersion:0),
       .Package(url:"https://github.com/iachievedit/swiftlog", majorVersion:1)
     ]
 )
